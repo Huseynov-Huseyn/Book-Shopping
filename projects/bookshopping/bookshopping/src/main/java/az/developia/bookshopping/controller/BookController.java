907fe4676834b7b7b1b082b5cc68c9d3133617c8
Hüseyn <huseynhuseyn343@gmail.com>
2024-04-08 01:20:11 +0400

    Lesson 57(Showing username of account owner)
    Login olan istifadəçi adının istifadəsi

--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import az.developia.bookshopping.config.MySession;
 import az.developia.bookshopping.dao.BookDAO;
 import az.developia.bookshopping.model.Book;
 import jakarta.validation.Valid;
@@ -21,6 +22,9 @@ public class BookController {
 
 	@Autowired
 	private BookDAO bookDAO;
+
+	@Autowired
+	private MySession mySession;
 
 	@GetMapping(path = "/books")
 	public String showBooks(Model model) {
@@ -43,7 +47,7 @@ public class BookController {
 			return "new-book";
 		}
 		book.setImage("book.jpg");
-		book.setUsername("DEA");
+		book.setUsername(mySession.getUsername());
 		bookDAO.save(book);
 		List<Book> books = bookDAO.findAll();
 		model.addAttribute("books", books);
--- projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
+++ projects/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import az.developia.bookshopping.config.MySession;
 import az.developia.bookshopping.dao.BookDAO;
 import az.developia.bookshopping.model.Book;
 
@@ -16,10 +17,14 @@ public class CustomerController {
 	@Autowired
 	private BookDAO bookDAO;
 
+	@Autowired
+	private MySession mySession;
+
 	@GetMapping(path = "/customer")
 	public String showCustomerPage(Model model) {
 		List<Book> books = bookDAO.findAll();
 		model.addAttribute("books", books);
+		System.out.println(mySession.getUsername());
 		return "customer";
 	}
 
